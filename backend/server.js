429a9d3244eb8846960e90fb2743fc781ab31f71
salman <salmankareem9@gmail.com>
2025-02-20 22:13:22 +0000

    server.js file update

--- backend/server.js
+++ backend/server.js
@@ -3,36 +3,40 @@ import axios from "axios";
 import cors from "cors";
 import "dotenv/config";
 
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const NASA_API_KEY = process.env.NASA_API_KEY;
 const NASA_API_URL = process.env.NASA_API_URL;
-app.use(cors({ origin: "https://nasa-neo-project-frontend.onrender.com" }));
+
+const corsOptions = {
+  origin: "https://nasa-neo-project-frontend.onrender.com",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send({ message: "API is running successfully " });
+    res.send({ message: "API is running successfully" });
 });
-
 
 app.get("/neo", async (req, res) => {
     try {
-       
         const today = new Date();
         const startDate = new Date();
-        startDate.setDate(today.getDate() - 3); 
+        startDate.setDate(today.getDate() - 3);
         const formattedStartDate = startDate.toISOString().split("T")[0];
         const formattedEndDate = today.toISOString().split("T")[0];
 
-       
         const { data } = await axios.get(
             `${NASA_API_URL}?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`
         );
+
         if (!data || !data.near_earth_objects) {
             throw new Error("Invalid API response");
         }
+
         res.json(data);
     } catch (error) {
         console.error("Error fetching Near-Earth Objects:", error.message);
@@ -40,4 +44,4 @@ app.get("/neo", async (req, res) => {
     }
 });
 
-app.listen(PORT,'0.0.0.0', () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running at http://localhost:${PORT}`));
