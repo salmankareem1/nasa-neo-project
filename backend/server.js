d6d2b78595f57ef1f1327f1dc4920e915614550f
salman <salmankareem9@gmail.com>
2025-02-20 17:34:32 +0000

    Removed embedded Git repo and added backend files correctly

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import "dotenv/config";
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const NASA_API_KEY = process.env.NASA_API_KEY;
+const NASA_API_URL = process.env.NASA_API_URL;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/neo", async (req, res) => {
+    try {
+       
+        const today = new Date();
+        const startDate = new Date();
+        startDate.setDate(today.getDate() - 3); 
+
+        const formattedStartDate = startDate.toISOString().split("T")[0];
+        const formattedEndDate = today.toISOString().split("T")[0];
+
+       
+        const { data } = await axios.get(
+            `${NASA_API_URL}?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`
+        );
+        if (!data || !data.near_earth_objects) {
+            throw new Error("Invalid API response");
+        }
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching Near-Earth Objects:", error.message);
+        res.status(500).json({ error: "Failed to fetch data from NASA API" });
+    }
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
