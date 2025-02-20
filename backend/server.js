735930ef9d34f42e48b3c9590fba35f1eccd84f7
salman <salmankareem9@gmail.com>
2025-02-20 21:57:45 +0000

    cors error update

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const NASA_API_KEY = process.env.NASA_API_KEY;
 const NASA_API_URL = process.env.NASA_API_URL;
-
-
-app.use(cors());
+app.use(cors({ origin: "https://nasa-neo-project-frontend.onrender.com" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
