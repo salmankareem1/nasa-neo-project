d14eeaafaa10c3e0d607775ebdafa5824ca082da
salman <salmankareem9@gmail.com>
2025-02-20 22:51:19 +0000

    update baend

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,21 @@ import axios from "axios";
 import cors from "cors";
 import "dotenv/config";
 
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const NASA_API_KEY = process.env.NASA_API_KEY;
 const NASA_API_URL = process.env.NASA_API_URL;
 
-const corsOptions = {
-  origin: "https://nasa-neo-project-frontend.onrender.com",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send({ message: "API is running successfully" });
+    res.send({ message: "API is running successfully " });
 });
+
 
 app.get("/neo", async (req, res) => {
     try {
@@ -29,9 +27,13 @@ app.get("/neo", async (req, res) => {
         const formattedStartDate = startDate.toISOString().split("T")[0];
         const formattedEndDate = today.toISOString().split("T")[0];
 
+        console.log("Fetching NEO data from:", `${NASA_API_URL}?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`);
+
         const { data } = await axios.get(
             `${NASA_API_URL}?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`
         );
+
+        console.log("NASA API Response:", data);
 
         if (!data || !data.near_earth_objects) {
             throw new Error("Invalid API response");
@@ -40,8 +42,9 @@ app.get("/neo", async (req, res) => {
         res.json(data);
     } catch (error) {
         console.error("Error fetching Near-Earth Objects:", error.message);
-        res.status(500).json({ error: "Failed to fetch data from NASA API" });
+        res.status(500).json({ error: `Failed to fetch data from NASA API: ${error.message}` });
     }
 });
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running at http://localhost:${PORT}`));
+
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running at http://localhost:${PORT}`));
